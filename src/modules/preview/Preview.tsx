7c437558bef29e3f27a33619beb076c99db59783
Kelvin Ward <kelvinward1010@gmail.com>
2024-02-10 19:31:42 +0700

    update

--- src/modules/preview/Preview.tsx
+++ src/modules/preview/Preview.tsx
@@ -1,9 +1,14 @@
+import { Blockquote } from '@mantine/core';
 import styles from './style.module.scss';
+import { IconInfoCircle } from '@tabler/icons-react';
 
 export function Preview() {
+    const icon = <IconInfoCircle />;
     return (
         <div className={styles.container}>
-            Preview
+            <Blockquote color="blue" cite="- Kelvin Ward" icon={icon} mt="xl">
+                Create with your favorite, awesome!
+            </Blockquote>
         </div>
     )
 }
