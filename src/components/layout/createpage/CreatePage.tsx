2acf648e793f949ecb756a40faa24db39f4a8b6e
Kelvin Ward <kelvinward1010@gmail.com>
2024-02-16 14:31:02 +0700

    update

--- src/components/layout/createpage/CreatePage.tsx
+++ src/components/layout/createpage/CreatePage.tsx
@@ -1,13 +1,16 @@
-import { IconFile, IconFolder } from '@tabler/icons-react';
+import { IconFile, IconFolder, IconHome } from '@tabler/icons-react';
 import styles from './style.module.scss';
 import { ActionIcon, Group, Text, Tooltip } from '@mantine/core';
 import { useAuth } from '../../../context/authContext';
 import { addDoc, collection, doc, setDoc } from 'firebase/firestore';
 import { database } from '../../../lib/firebase';
+import { useNavigate } from 'react-router-dom';
+import { previewUrl } from '../../../urls';
 
 export const CreatePage: React.FC = () => {
 
     const user = useAuth();
+    const navigate = useNavigate();
 
     const handleCreateNewFolder = async (type: string) => {
 
@@ -32,7 +35,16 @@ export const CreatePage: React.FC = () => {
         <div className={styles.container}>
             <Text>Tools</Text>
             <Group gap={5}>
-                <Tooltip label="Folder" openDelay={500} closeDelay={100}>
+                <Tooltip label="Home" openDelay={500} closeDelay={100}>
+                    <ActionIcon                    
+                        variant="default"
+                        size={35}
+                        onClick={() =>navigate(previewUrl)}
+                    >
+                        <IconHome size={16}/>
+                    </ActionIcon>
+                </Tooltip>
+                <Tooltip label="Create folder" openDelay={500} closeDelay={100}>
                     <ActionIcon                    
                         variant="default"
                         size={35}
@@ -41,7 +53,7 @@ export const CreatePage: React.FC = () => {
                         <IconFolder size={16}/>
                     </ActionIcon>
                 </Tooltip>
-                <Tooltip label="File" openDelay={500} closeDelay={100}>
+                <Tooltip label="Create file" openDelay={500} closeDelay={100}>
                     <ActionIcon                    
                         variant="default"
                         size={35}
